381349d9d0255e5bc77eeac09a00203a8cbc9f83
w0rm5 <47029568+w0rm5@users.noreply.github.com>
2022-10-08 20:52:32 +0700

    Initial commit

---
+++ popup/popup.js
@@ -0,0 +1,39 @@
+const codeForm = document.getElementById("codeForm");
+
+if(codeForm) {
+    codeForm.onsubmit = function(e) {
+        e.preventDefault();
+
+        let marsUrl = document.getElementById("mars_url").value;
+        let gpid = document.getElementById("gpid").value;
+        let gameId = document.getElementById("game_id").value;
+        let device = document.getElementById("device").value;
+        let langauge = document.getElementById("lang").value;
+
+        if(!marsUrl.trim() || isNaN(gpid) || isNaN(gameId)) {
+            return;
+        }
+
+        if(marsUrl.startsWith("//")) {
+            marsUrl = marsUrl.substring(2);
+        }
+
+        let url = "http://" + marsUrl + "&gpid=" + gpid + "&gameid=" + gameId + "&device=" + device + "&lang=" + langauge;
+
+        let launchInPrivate = document.getElementById("private_browser").checked;
+
+        if(launchInPrivate){
+            chrome.windows.getAll({populate: false, windowTypes: ['normal']}, function(windows) {
+                for (let w of windows) {
+                    if (w.incognito) {
+                        chrome.tabs.create({url: url, windowId: w.id});
+                        return;
+                    }
+                }
+                chrome.windows.create({url: url, incognito: true});
+            });
+            return;
+        }
+        chrome.tabs.create({url: url});
+    }
+}
